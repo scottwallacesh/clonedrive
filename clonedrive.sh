287b6ec564a9d442b4a5b02b49729c4d333bb0b3
Scott Wallace <scott@wallace.sh>
2017-08-06 09:11:43 +0100

    Re-jig the logic of the script.

--- clonedrive.sh
+++ clonedrive.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-MASTER_MOUNT=/Volumes/Downloads
+MASTR_MOUNT=/Volumes/Downloads
 GOOGL_MOUNT=~/mnt/GoogleDriveCrypt
 CACHE_MOUNT=~/mnt/cache
 UNION_MOUNT=~/mnt/union
 
-# Simple function
+# Simple function to test if a directory is in use
 function in_use() {
-    MOUNT=${!}
+    MOUNT=${1}
 
     lsof ${MOUNT} > /dev/null 2>&1
 
@@ -17,21 +17,31 @@ function in_use() {
 # Ensure the mountpoints exist
 mkdir -p ${CACHE_MOUNT} ${GOOGL_MOUNT} ${UNION_MOUNT}
 
-# Try to umount the mountpoints to reset and exit if not
-umount ${UNION_MOUNT} ${GOOGL_MOUNT} 2> /dev/null
-in_use ${UNION_MOUNT} || exit 1
-in_use ${GOOGL_MOUNT} || exit 2
+while true; do
+    # Try to umount the mountpoints to reset and exit if not
+    umount ${UNION_MOUNT} ${GOOGL_MOUNT} 2> /dev/null
+    in_use ${UNION_MOUNT} && exit 1
+    in_use ${GOOGL_MOUNT} && exit 2
 
+    # Mount Google Drive, read-only
+    rclone mount --read-only \
+                 --allow-other \
+                 --no-modtime \
+                 --dir-cache-time=10s \
+                 --buffer-size=1G \
+                 -v \
+                 GoogleDriveCrypt: ${GOOGL_MOUNT} &
 
-# Mount the directories
-rclone mount --read-only \
-             --dir-cache-time=10s \
-             --buffer-size=1G \
-             GoogleDriveCrypt: ${GOOGL_MOUNT} &
-unionfs -o cow ${GOOGL_MOUNT}=RO:${MASTER_MOUNT}=RO:${CACHE_MOUNT}=RW ${UNION_MOUNT}
+    # UnionFS magic
+    unionfs -o cow ${GOOGL_MOUNT}=RO:${MASTR_MOUNT}=RO:${CACHE_MOUNT}=RW ${UNION_MOUNT}
 
-# Ensure local cache to moved to The Cloud regularly
+    wait
+done &
+
+# Ensure local cache is moved to The Cloud regularly
 while true; do
     cd ${CACHE_MOUNT} && rclone move . GoogleDriveCrypt:
     sleep 21600 # 6 hours
 done
+
+exit 0
